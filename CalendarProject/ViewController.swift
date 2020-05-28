c024db57b38b8a7f5ca6c8705c5d7b1f81dc76c1
Tayler Moosa <bodhimoosa@gmail.com>
2020-05-27 20:31:30 -0400

    Initial Commit

---
+++ CalendarProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarProject
+//
+//  Created by Tayler Moosa on 5/27/20.
+//  Copyright © 2020 Tayler Moosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
